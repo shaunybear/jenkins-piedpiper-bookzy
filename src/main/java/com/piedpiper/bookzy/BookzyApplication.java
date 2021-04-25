fce4c536487b29a3b61f57ce4b38b47f3c1a0d66
Shaun Nelson <shaunybear.inc@gmail.com>
2021-04-25 15:49:21 -0400

    first commit

---
+++ src/main/java/com/piedpiper/bookzy/BookzyApplication.java
@@ -0,0 +1,13 @@
+package com.piedpiper.bookzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookzyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookzyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/piedpiper/bookzy/BookzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piedpiper.bookzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
