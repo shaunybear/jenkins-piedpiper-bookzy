f71eb4373cf4b07535a7ee2882dab51dc43d15ca
Shaun Nelson <shaunybear.inc@gmail.com>
2021-04-26 13:31:34 -0400

    print hello world endpoint

--- src/main/java/com/piedpiper/bookzy/BookzyApplication.java
+++ src/main/java/com/piedpiper/bookzy/BookzyApplication.java
@@ -2,12 +2,20 @@ package com.piedpiper.bookzy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+
 public class BookzyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookzyApplication.class, args);
 	}
 
+	@RequestMapping(value = "/")
+	public String hello() {
+		return "Hello World";
+	}
 }
